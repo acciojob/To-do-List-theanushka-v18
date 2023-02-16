9e8a47a29702a89c6e26fad91611fc925ef261ad
Anushka Verma <anushkaverma779@gmail.com>
2023-02-16 15:45:34 +0530

    completed

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
 
+let addTodoBtn = document.getElementById("addTodoBtn");
+
+addTodoBtn.addEventListener("click", function() {
+    let todoText = document.getElementById("newTodoInput").value;
+    if (todoText != "") {
+        let li = document.createElement("li");
+        li.innerHTML = todoText;
+        document.getElementById("todoList").appendChild(li);
+        document.getElementById("newTodoInput").value = "";
+    }
+})
